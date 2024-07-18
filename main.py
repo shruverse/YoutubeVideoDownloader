bb6974bc8bf4a76128898923087ba1609700075f
shruverse <151932197+shruverse@users.noreply.github.com>
2024-07-18 10:49:47 +0530

    Responsive styling

--- main.py
+++ main.py
@@ -6,14 +6,9 @@ app = Flask(__name__)
 
 def estimate_file_size(duration, bitrate_kbps=8000):
     duration_seconds = int(duration)
-    file_size_bytes = bitrate_kbps * duration_seconds * 1000 / 8
-    file_size_mb = file_size_bytes / (1024 * 1024)
-    return file_size_mb
-
-
-def format_file_size(file_size_mb):
+    file_size_mb = bitrate_kbps * duration_seconds / 8 / 1024
     return f"{file_size_mb:.2f}"
-
+    
 
 @app.route('/')
 def home():
@@ -22,20 +17,19 @@ def home():
 
 @app.route('/search', methods=['POST'])
 def search():
-    data = request.get_json()
-    video_url = data.get('video_url', '')
-
+    video_url = request.get_json().get('video_url')
     try:
         yt = YouTube(video_url)
         response = {
             'thumbnail_url': yt.thumbnail_url,
             'title': yt.title,
-            'file_size_mb': format_file_size(estimate_file_size(yt.length))
+            'file_size_mb': estimate_file_size(yt.length)
         }
         return jsonify(response)
 
     except Exception as e:
         return jsonify({'error': str(e)}), 400
+
 
 
 if __name__ == "__main__":
